fc2da1a4a81e36af1961922c72fb676793529997
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2021-06-14 10:50:32 -0500

    add help to matlab wrapper

--- quakeio.m
+++ quakeio.m
@@ -1,4 +1,8 @@
 function GroundMotion = quakeio(varargin)
 [status, output] = system(strjoin({'quakeio', strjoin(varargin)}));
-GroundMotion = jsondecode(output);
+if strcmp(varargin{1},"-h") || strcmp(varargin{1},"--help")
+  output
+else
+  GroundMotion = jsondecode(output);
+end
 end
