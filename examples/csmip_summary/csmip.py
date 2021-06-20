cd9364bd617555c8b648b4d6e53aa7fcf74a3e61
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2021-06-20 10:59:43 -0700

    cleaning summary example

--- examples/csmip_summary/csmip.py
+++ examples/csmip_summary/csmip.py
@@ -1,16 +1,73 @@
+# Claudio Perez
+
 import sys
 from collections import defaultdict
 
+import yaml
 import numpy as np
 
 import quakeio
 
+def consolidate_records(data):
+    #summary = defaultdict(lambda: defaultdict(dict))
+    summary = defaultdict(dict)
+    for loc, record in data.items():
+        for component in record.values():
+            del component["file_name"]
+            del component["instr_period"]
+            del component["instr_period.units"]
+            del component["peak_accel.units"]
+            del component["peak_veloc.units"]
+            del component["peak_displ.units"]
+            del component["init_veloc.units"]
+            del component["init_displ.units"]
+            del component["filter.bandpass.point.units"]
+            del component["filter.bandpass.limit.units"]
+            chan          = component.pop("channel")
+            station_chan  = component.pop("station.channel")
+            chan = station_chan if station_chan else chan
+            station_no    = component.pop("station.no")
+            station_coord = component.pop("station.coord")
+            filter_low    = component.pop("filter.bandpass.limit_low")
+            filter_high   = component.pop("filter.bandpass.limit_high")
+            filter_point  = component.pop("filter.bandpass.point")
+            loc  = component.pop("location") 
+            comp = component.pop("component")
+            summary[loc][comp+f" (Chan {chan})"] = \
+                    component.serialize(
+                            ljust=35, 
+                            serialize_series=False,
+                            humanize_keys=True, 
+                            summarize=True
+                    )
+
+    station_data = {}
+    station_data["Station Number"] = station_no
+    station_data["Station Coordinates"] = station_coord
+    station_data["Filter point"] = filter_point
+    station_data["Filter low cut"] = filter_low
+    station_data["Filter high cut"] = filter_high
+    return f"""
+{yaml.dump(station_data)}
+
+## Records
+
+```yaml
+{yaml.dump(dict(summary))}
+```
+
+"""
+
+
 def update_peaks(peaks, record):
+    """
+    Helper function which updates peaks in `peak` with
+    values from `record`
+    """
     for attr in ["peak_accel", "peak_veloc", "peak_displ"]:
         peaks[attr] = max(record[attr], peaks[attr], key=abs)
 
-def summarize(file_name, bridge_channels, ground_channels):
-    data = quakeio.read(file_name, "csmip.zip", summarize=True)
+def extract_peaks(data, bridge_channels, ground_channels):
 
     # create a data structure whose default field is another data
     # structure whose default field is zero
@@ -34,25 +91,28 @@ def summarize(file_name, bridge_channels, ground_channels):
 
     return bridge_records, ground_records
 
-def gen_summary(file_name, brdg, site):
+def summary_B(file_name, brdg, grnd):
+    g = 9.80665 # m/s^2
     return f"""
 # Summary for event file `{file_name}`
 
 ## Bridge Channels
 
 | Component    | Acceleration | Velocity  | Displacement |
+|              |      g       |   cm/s    |     cm       |
 |--------------|--------------|-----------|--------------|
-| Longitudinal | {brdg["long"]["peak_accel"]:12.4f} | {brdg["long"]["peak_veloc"]:9.4f} | {brdg["long"]["peak_displ"]:12.4f} |
-| Transverse   | {brdg["tran"]["peak_accel"]:12.4f} | {brdg["tran"]["peak_veloc"]:9.4f} | {brdg["tran"]["peak_displ"]:12.4f} |
-| Vertical     | {brdg["vert"]["peak_accel"]:12.4f} | {brdg["vert"]["peak_veloc"]:9.4f} | {brdg["vert"]["peak_displ"]:12.4f} |
+| Longitudinal | {1/(g*100)*brdg["long"]["peak_accel"]:12.4f} | {brdg["long"]["peak_veloc"]:9.4f} | {brdg["long"]["peak_displ"]:12.4f} |
+| Transverse   | {1/(g*100)*brdg["tran"]["peak_accel"]:12.4f} | {brdg["tran"]["peak_veloc"]:9.4f} | {brdg["tran"]["peak_displ"]:12.4f} |
+| Vertical     | {1/(g*100)*brdg["up"]["peak_accel"]:12.4f} | {brdg["up"]["peak_veloc"]:9.4f} | {brdg["up"]["peak_displ"]:12.4f} |
 
 ## Site Channels
 
 | Component    | Acceleration | Velocity  | Displacement |
+|              |      g       |   cm/s    |     cm       |
 |--------------|--------------|-----------|--------------|
-| Longitudinal | {grnd["long"]["peak_accel"]:12.4f} | {grnd["long"]["peak_veloc"]:9.4f} | {grnd["long"]["peak_displ"]:12.4f} |
-| Transverse   | {grnd["tran"]["peak_accel"]:12.4f} | {grnd["tran"]["peak_veloc"]:9.4f} | {grnd["tran"]["peak_displ"]:12.4f} |
-| Vertical     | {grnd["vert"]["peak_accel"]:12.4f} | {grnd["vert"]["peak_veloc"]:9.4f} | {grnd["vert"]["peak_displ"]:12.4f} |
+| Longitudinal | {1/(g*100)*grnd["long"]["peak_accel"]:12.4f} | {grnd["long"]["peak_veloc"]:9.4f} | {grnd["long"]["peak_displ"]:12.4f} |
+| Transverse   | {1/(g*100)*grnd["tran"]["peak_accel"]:12.4f} | {grnd["tran"]["peak_veloc"]:9.4f} | {grnd["tran"]["peak_displ"]:12.4f} |
+| Vertical     | {1/(g*100)*grnd["up"]["peak_accel"]:12.4f} | {grnd["up"]["peak_veloc"]:9.4f} | {grnd["up"]["peak_displ"]:12.4f} |
 
 """
 
@@ -66,7 +126,9 @@ if __name__ == "__main__":
     ground_channels = [1, 2, 3, 6, 7, 17, 18, 24, 25],
     bridge_channels = [11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23]
 
-    brdg, grnd = summarize(file_name, bridge_channels, ground_channels)
+    event = quakeio.read(file_name, "csmip.zip", summarize=True)
+    brdg, grnd = extract_peaks(event, bridge_channels, ground_channels)
 
-    print(gen_summary(file_name, brdg, grnd))
+    print(summary_B(file_name, brdg, grnd))
+    print(consolidate_records(event))
 
