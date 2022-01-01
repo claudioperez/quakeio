a3d182c54c85891d1bc80e781098df649aec22ee
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2021-12-31 19:13:42 -0800

    cmp - fix basic read example

--- examples/basic_read.py
+++ examples/basic_read.py
@@ -1,10 +1,13 @@
 import quakeio
 
-csmip_event = quakeio.read("event.zip")
-csmip_event["channel-01"].accel.plot()
-
-csmip_event["chan001"].plot()
-
-csmip_event["chan001"].plot_spect()
+csmip_event = quakeio.read("dat/58658_007_20210426_10.09.54.P.zip")
+record = csmip_event["bent_4_north_column_grnd_level"]
+component = record["long"]
+series = component.accel
+series.plot()
+#
+#csmip_event["chan001"].plot()
+#
+#csmip_event["chan001"].plot_spect()
 
 
