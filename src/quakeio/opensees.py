f7525ce607cea168555c3810861fad9221d66e59
claudio perez <50180406+claudioperez@users.noreply.github.com>
2022-03-13 21:21:21 -0700

    cmp - add tcl output format

--- src/quakeio/opensees.py
+++ src/quakeio/opensees.py
@@ -11,15 +11,41 @@ def read(read_file, **kwds):
 def write(write_file, ground_motion, **kwds):
     # print(type(ground_motion))
     if isinstance(ground_motion, QuakeComponent):
+        dt = ground_motion.accel["time_step"]
         accel = numpy.asarray(ground_motion.accel.data)[:, None]
     elif isinstance(ground_motion, QuakeSeries):
+        dt = ground_motion["time_step"]
         accel = numpy.asarray(ground_motion.data)[:, None]
     else:
         raise ValueError(
             f"Cannot conver motion of type `{type(ground_motion)}` to QuakeComponent"
         )
     with open_quake(write_file, "w") as f:
-        numpy.savetxt(f, accel, fmt="%.8f")  # delimiter="\n")
+        #numpy.savetxt(f, accel, fmt="%.8f")  # delimiter="\n")
+        f.write(f"-dt {dt} -values {{")
+        numpy.savetxt(f, accel, fmt="%.8f", delimiter=" ")
+        f.write("}")
+
+def write_tcl(write_file, ground_motion, **kwds):
+    if isinstance(ground_motion, QuakeComponent):
+        series = ground_motion.accel
+    elif isinstance(ground_motion, QuakeSeries):
+        series = ground_motion
+    else:
+        raise ValueError(
+            f"Cannot conver motion of type `{type(ground_motion)}` to QuakeComponent"
+        )
+    with open_quake(write_file, "w") as f:
+        f.write(f"dt {series['time_step']} ")
+        for k,v in series.items():
+            if isinstance(v,(str,int,float)):
+                f.write(f"{k} {v} ")
+        f.write(f"values {{")
+        numpy.savetxt(f, series.data, fmt="%.8f", delimiter=" ")
+        f.write("}")
 
 
-FILE_TYPES = {"opensees": {"write": write, "type": "any"}}
+FILE_TYPES = {
+    "opensees": {"write": write, "type": "any"},
+    "tcl":      {"write": write_tcl, "type": "any"}
+}
